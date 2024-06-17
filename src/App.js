78eaae65dce6b9b2a78abe94f60adfcd3ee19fb9
gkelshelashvili <giorgikesho6@gmail.com>
2024-06-17 14:10:33 +0400

    startup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div class="main_wraper">
+        <div class="card">
+            <h4 class="advice">Advice #92</h4>
+            <p class="main_text">dummy text</p>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
